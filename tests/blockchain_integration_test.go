6c8e00eea9de6608f5ca43379afbf259b722450d
Chepkwony Peter <oalliancekenya@gmail.com>
2025-05-13 23:53:15 +0300

    Add tests/blockchain_integration_test.go 1747169594854

---
+++ tests/blockchain_integration_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"testing"
+	// Import other necessary packages for integration testing
+)
+
+func TestBlockchainIntegration(t *testing.T) {
+	// TODO: Implement blockchain integration tests
+	t.Log("Blockchain integration test placeholder")
+}
